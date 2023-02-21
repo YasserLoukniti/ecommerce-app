2a76abaa9cb5ac6d25630e6dbf6cd95053407ee5
YL <yass_official@outlook.fr>
2023-02-22 00:51:39 +0100

    Add Modt Ordered Section

---
+++ src/data/ProductsData.js
@@ -0,0 +1,36 @@
+
+export const productsData = [
+  {
+    id: 1,
+    title: 'Bulerie varoise',
+    prix: 10,
+    categorie: 'bonbon coca',
+    color:'#c1aeae',
+    image:require("../../assets/cannabisBonon.jpg")
+  },
+  {
+    id: 2,
+    title: 'Product 2',
+    prix: 10,
+    categorie: 'bonbon banane',
+    color:'#a3b5a2',
+    image:require("../../assets/cannabisBonon.jpg")
+  },
+  {
+    id: 3,
+    title: 'Product 3',
+    prix: 30,
+    categorie: 'bonbon fraise',
+    color:'#a3b5a2',
+    image:require("../../assets/cannabisBonon.jpg")
+  },
+  {
+    id: 4,
+    title: 'Product 4',
+    prix: 12,
+    categorie: 'bonbon milka',
+    color:'#a3b5a2',
+    image:require("../../assets/cannabisBonon.jpg")
+  },
+  
+];
