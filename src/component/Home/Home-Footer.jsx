398ea5c027f868df5fbaa1f9e05fcb0f1f7b488d
YL <yass_official@outlook.fr>
2023-02-23 00:47:03 +0100

    Add Footer

--- src/component/Home/Home-Footer.jsx
+++ src/component/Home/Home-Footer.jsx
@@ -1,19 +1,41 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { Image, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import { Button } from 'react-native-elements';
 
 export default function HomeFooter() {
 
     return (
         <View style={styles.footer}>
-        <Text>ssss</Text>
-    </View>
+            <View style={styles.home}>
+                <TouchableOpacity onPress={console.log("clicked")}>
+                    <Image source={require("../../../assets/homeIcon.png")}></Image>
+                </TouchableOpacity>
+            </View>
+            <View style={styles.panier}>
+            <TouchableOpacity onPress={console.log("clicked")}>
+                    <Image source={require("../../../assets/panierIcon.png")}></Image>
+                </TouchableOpacity>
+            </View>
+        </View>
     );
 }
 
 const styles = StyleSheet.create({
     footer: {
-        flex: 2,
-        backgroundColor: "red",
+        flex: 1,
+        flexDirection: 'row',
+        flexWrap: 'wrap',
+        justifyContent: 'space-between',
         alignItems: 'center',
-        justifyContent: 'center',
+        marginBottom:10
+    },
+    home: {
+        flex: 1,
+        paddingLeft:40
+    },
+    panier: {
+        flex: 1,
+        alignItems: 'flex-end',
+        paddingRight:40
+
     },
 });
