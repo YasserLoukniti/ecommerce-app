093bcba6074f01b09b2efc1f2b724c6f13706a23
Ilyes98 <belfekihilyess@gmail.com>
2023-02-23 02:15:56 +0100

    Add product detail

---
+++ src/screen/Data/data.jsx
@@ -0,0 +1,19 @@
+const Products = [
+  {
+    id: 1,
+    name: 'Product 1',
+    price: 10,
+    //image: 'https://example.com/product1.jpg',
+    description: 'This is product 1.'
+  },
+  {
+    id: 2,
+    name: 'Product 2',
+    price: 20,
+    //image: 'https://example.com/product2.jpg',
+    description: 'This is product 2.'
+  },
+  // add more products as needed
+];
+
+export default Products;
